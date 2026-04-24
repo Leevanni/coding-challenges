3b78bc652d28f041c469b597d3303fe7c0c4fc9d
Leevanni Goncalves <leevannigoncalves@gmail.com>
2026-04-23 21:02:48 -0400

    added searchFirstPosition variation of binarySearch

--- src/main/java/com/example/challenges/CodingQuestions.java
+++ src/main/java/com/example/challenges/CodingQuestions.java
@@ -37,28 +37,90 @@ public class CodingQuestions {
     	return -1;
     }
     
-    
-    public static int binarySearch(int[] num, int target) {
-    	
-    	int left  = 0;
-    	int right = num.length - 1;
+
+    public int binarySearchTree(int[] nums, int target) {
+    	int left = 0;
+    	int right = nums.length - 1;
     	
     	while (left <= right) {
-    		
     		int mid = left + (right - left) / 2;
     		
-    		if (num[mid] == target) {
+    		if (nums[mid] == target) {
     			return mid;
     		}
     		
-    		if (num[mid] < target) {
+    		if (nums[mid] < target) {
     			left = mid + 1;
-    			
     		} else {
     			right = mid - 1;
     		}
     	}
     	
-    	return -1;
+    	return -1; // Value not found
     }
+    
+    public int searchFirstPosition(int[] nums, int target) {
+    	int left = 0;
+    	int right = nums.length - 1;
+    	
+    	while (left <= right) {
+    		int mid = left + (right - left) / 2;
+    		
+    		if (nums[mid] == target) {
+    			return mid;
+    		}
+    		
+    		if (nums[mid] < target) {
+    			left = mid + 1;
+    		} else {
+    			right = mid - 1;
+    		}
+    	}
+    	
+    	return left; 
+    }
+    
+    public int firstBadVerstion(int n) {
+    	
+    	
+    	int left = 1;
+    	int right = n;
+    	
+    	while (left < right) {
+    		
+    		int mid = left + (right - left) / 2;
+    		
+    		if (isBadVersion(mid)) {
+    			right = mid;
+    		} else {
+    			left = mid + 1;
+    		}
+    	}
+    	
+    	return left;
+    }
+
+	public boolean isBadVersion(int mid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+	public int findPeakElement(int[] nums) {
+		int left = 0;
+		int right = nums.length - 1;
+		
+		
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			
+			if (nums[mid] < nums[mid + 1]) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		
+		return left;
+	}
 }
