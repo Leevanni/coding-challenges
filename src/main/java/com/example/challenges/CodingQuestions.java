c0f68fb319e5c57fb4344857b59ba75a736146b1
Leevanni Goncalves <leevannigoncalves@gmail.com>
2026-04-26 22:34:44 -0400

    reverse a string in java

--- src/main/java/com/example/challenges/CodingQuestions.java
+++ src/main/java/com/example/challenges/CodingQuestions.java
@@ -41,26 +41,26 @@ public class CodingQuestions {
     }
     
 
-    public int binarySearchTree(int[] nums, int target) {
-    	int left = 0;
-    	int right = nums.length - 1;
-    	
-    	while (left <= right) {
-    		int mid = left + (right - left) / 2;
-    		
-    		if (nums[mid] == target) {
-    			return mid;
-    		}
-    		
-    		if (nums[mid] < target) {
-    			left = mid + 1;
-    		} else {
-    			right = mid - 1;
-    		}
-    	}
-    	
-    	return -1; // Value not found
-    }
+//    public int binarySearchTree(int[] nums, int target) {
+//    	int left = 0;
+//    	int right = nums.length - 1;
+//    	
+//    	while (left <= right) {
+//    		int mid = left + (right - left) / 2;
+//    		
+//    		if (nums[mid] == target) {
+//    			return mid;
+//    		}
+//    		
+//    		if (nums[mid] < target) {
+//    			left = mid + 1;
+//    		} else {
+//    			right = mid - 1;
+//    		}
+//    	}
+//    	
+//    	return -1; // Value not found
+//    }
     
     public int searchFirstPosition(int[] nums, int target) {
     	int left = 0;
@@ -107,25 +107,6 @@ public class CodingQuestions {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	
-//	public int findPeakElement(int[] nums) {
-//		int left = 0;
-//		int right = nums.length - 1;
-//		
-//		
-//		while (left < right) {
-//			int mid = left + (right - left) / 2;
-//			
-//			if (nums[mid] < nums[mid + 1]) {
-//				left = mid + 1;
-//			} else {
-//				right = mid;
-//			}
-//		}
-//		
-//		return left;
-//	}
 
 	
 	public int findPeakElement(int[] nums) {
@@ -229,5 +210,21 @@ public class CodingQuestions {
 		
 		return answer;
 	}
-
+	
+	
+	public void fixedSlidingWindow(int[] nums, int windowSize) {
+		
+	}
+	
+	
+	public String reverseString(String string) {
+		
+		String str = "";
+		
+		for (int i = 0; i < string.length(); i++) {
+			str = string.charAt(i) + str;
+		}
+		
+		return str;
+	}
 }
