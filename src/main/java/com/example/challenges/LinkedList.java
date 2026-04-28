a07cf8419f58bcaaf2ebc82d403edbbef1605f51
Leevanni Goncalves <leevannigoncalves@gmail.com>
2026-04-27 21:32:27 -0400

    implement append method

--- src/main/java/com/example/challenges/LinkedList.java
+++ src/main/java/com/example/challenges/LinkedList.java
@@ -8,4 +8,26 @@ public class LinkedList {
 		newNode.next = head;
 		head = newNode;
 	}
+	
+	public void append(int data) {
+		
+		Node newNode = new Node(data);
+		
+		if (head == null ) {
+			head = newNode;
+			return;
+		}
+		
+		Node curr = head;
+		
+		while (curr.next != null) {
+			curr = curr.next;
+			
+			if (curr.next == null ) {
+				curr = curr.next;
+			}
+		}
+		
+		curr.next = newNode;
+	}
 }
