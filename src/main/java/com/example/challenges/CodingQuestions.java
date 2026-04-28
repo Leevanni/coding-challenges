52804d57901621ea616eebface342d2dc7762c60
Leevanni Goncalves <leevannigoncalves@gmail.com>
2026-04-27 22:56:06 -0400

    best time to buy and sell problem

--- src/main/java/com/example/challenges/CodingQuestions.java
+++ src/main/java/com/example/challenges/CodingQuestions.java
@@ -227,4 +227,26 @@ public class CodingQuestions {
 		
 		return str;
 	}
+	
+	public int bestTimeToBuyAndSell(int[] prices) {
+		
+		int minPrice = prices[0];
+		int maxProfit = 0;
+		
+		for (int i = 1; i < prices.length; i++) {
+			
+			if (prices[i] < minPrice) {
+				minPrice = prices[i];
+			}
+			
+			int profit = prices[i] - minPrice;
+			
+			if (profit > maxProfit) {
+				maxProfit = profit;
+			}
+		}
+		
+		return maxProfit;
+		
+	}
 }
